bba4059ce51acad9e64372001f46654b0acacd6b
PeterHladky <petr.hladky@gmail.com>
2023-04-04 21:28:06 +0200

    [New}: fizzbuzz returns Fizz for 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,4 +5,8 @@ describe("fizzbuzz", () => {
         expect(typeof(fizzbuzz(5))).toBe('string');
     })
 
+    it('returns Fizz for 3',() => {
+        expect(fizzbuzz(3)).toEqual('Fizz');
+    } )
+
 });
