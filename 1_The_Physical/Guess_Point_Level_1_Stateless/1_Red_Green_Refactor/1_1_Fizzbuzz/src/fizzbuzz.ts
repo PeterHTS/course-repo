01c61f7c8de2cd06fab9057157a2456d0aa65d65
PeterHladky <petr.hladky@gmail.com>
2023-04-06 16:56:56 +0200

    [Refactor}: fizzbuzz returns Fizz for multiples of 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,6 @@
 export function fizzbuzz(number: number) {
-    if (number === 3) return 'Fizz';
+    if (number%3 === 0) return 'Fizz';
+    if (number === 5) return 'Buzz';
+    if(number ===1) return '1';
     return "";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -9,4 +9,16 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz(3)).toEqual('Fizz');
     } )
 
+    it('returns Buzz for 5',() => {
+        expect(fizzbuzz(5)).toEqual('Buzz');
+    })
+
+    it('returns string 1 for 1',() => {
+        expect(fizzbuzz(1)).toEqual('1');
+    })
+
+    it('returns Fizz for multiples of 3',() => {
+        expect(fizzbuzz(6)).toEqual('Fizz');
+    } )
+
 });
