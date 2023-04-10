f64e406f2f3b9715bc55656922853080bb2745b4
PeterHladky <petr.hladky@gmail.com>
2023-04-10 13:07:52 +0200

    isPalindrome method is defined

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,8 @@
+import {isPalindrome} from "./palindrome";
 
 describe('palindrome checker', () => {
-
+    it('should return true if the palindrome method is defined', () => {
+        expect(isPalindrome('mom')).toBeDefined;
+    })
 })
+
