d6b8168d2be7ae82b2eb0fa119d3beee999060c2
PeterHladky <petr.hladky@gmail.com>
2023-04-10 13:15:46 +0200

    [New]:isPalindrome returns true for mom

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,5 +4,13 @@ describe('palindrome checker', () => {
     it('should return true if the palindrome method is defined', () => {
         expect(isPalindrome('mom')).toBeDefined;
     })
+
+    it('should return true if input is mom', ()=> {
+        expect(isPalindrome('mom')).toBe(true);
+    })
+
+    it('should return false if the input is bill', ()=> {
+        expect(isPalindrome('bill')).toBe(false);
+    })
 })
 
